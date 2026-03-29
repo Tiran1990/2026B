93847823c23e4dd0de36dbe7d39f52787d44c93b
Tiran1990 <t.hesawi1990@gmail.com>
2026-03-29 09:56:53 +0300

    new Docs

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestThreadCheckArray {
+public class TestThreadCheckArray {//hello
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
